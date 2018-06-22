12c1257c30e68aa23dba9d8233fcdaff32ded263
Mustafa Nafees <mustafa.nafees@gmail.com>
2018-06-22 18:15:21 +0300

    adding keys.py file

---
+++ keys.py
@@ -0,0 +1,8 @@
+# Twitter consumer keys and access tokens. This information has to be kept secret. 
+# You can create a new application here https://twitter.com/settings/applications
+# once you generate these keys copy and paste them here
+
+consumer_key = 'your consumer key'
+consumer_secret = 'your consumer secret'
+access_token = 'your access token'
+access_token_secret = 'your access tokensecret'
